ba0bdbb01e7bfae0619a622e38df2c8f9733c3a8
Colby Gavin <colbyeg@gmail.com>
2020-02-13 11:01:13 -0500

    added layout

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import TrainInput from "./components/trainInput";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="jumbotron jumbotron-fluid">
+        <div class="container">
+          <h1 class="display-4">It's Time For Some TRAINS!!!!</h1>
+          <p class="lead">chugga chugga choo choo</p>
+        </div>
+      </div>
+      <div class="container">
+        <div class="row">
+          <div class="col-sm">Train Name</div>
+          <div class="col-sm">Destination</div>
+          <div class="col-sm">Frequency</div>
+          <div class="col-sm">Next Arrival</div>
+          <div class="col-sm">Minutes Away</div>
+        </div>
+      </div>
+      <TrainInput />
     </div>
   );
 }
---
+++ src/components/trainInput.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "../App.css";
+
+const TrainInput = () => {
+  return (
+    <form>
+      <div className="form-group"> </div>
+    </form>
+  );
+};
+export default TrainInput;
